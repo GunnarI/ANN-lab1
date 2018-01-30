36d96b431e1fde27006aeea31e6a26b4807f2e22
sinorious <35799399+sinorious@users.noreply.github.com>
2018-01-30 16:03:43 +0100

    Update ANN_project1.m

--- ANN_project1.m
+++ ANN_project1.m
@@ -3,10 +3,10 @@ close all
 clc
 
 % ANN Project 1
-% Create data which is then used for perceptron learning
+% Create data and imply single layer perceptron learning
 
 %%          3.1.1 create Datasets
-% Create 2 datasets of multivariant distribution(with mu and sigma)
+% Create 2 datasets of multivariant distribution (with mu and sigma)
 % with linearly seperable data (100 points per class)
 
 %rng default;                    %Will always produce the same randon data
@@ -14,33 +14,33 @@ clc
 % first group of data
 mu1 = [10,4];
 sigma1 = [10,1;1,10];
-data1 = mvnrnd(mu1,sigma1,100);
+data1 = mvnrnd(mu1,sigma1,100);  %Produces multivariant normal distributed data
 
 %second group of data
 mu2 = [-10,-4];
 sigma2 = [10,1;1,10];
-data2 = mvnrnd(mu2,sigma2,100);
+data2 = mvnrnd(mu2,sigma2,100);  %Produces multivariant normal distributed data
 
-% combine data into one matrix with bias line in input
+% combine data into one matrix and add bias line in input
 all_data = [data1; data2];
 all_data = all_data';
-bias = ones(1,200);
-all_data = [all_data; bias];
+all_data = [all_data; ones(1,200)]; %All data including the bias line
 
 % Create an output matrix
-target = [ones(1,100), -ones(1,100)];
+target = [ones(1,100), -ones(1,100)];   %first data group is 1 and second is -1
 
 % create a weight matrix
 [numDims, numInst] = size(all_data);
 numClasses = size(target,1);
-weights = zeros(numClasses, numDims);
+weights = rand(numClasses, numDims);
 delta_weight = zeros(numClasses, numDims);
 
-%%          Plotting
+%%          Plotting of data points
 
 figure(1)
 plot(all_data(1,1:100),all_data(2,1:100),'b+')
 grid on
+title('Data points for classification')
 xlabel('X')
 ylabel('Y')
 
@@ -54,46 +54,34 @@ plot(all_data(1,101:200),all_data(2,101:200),'r+')
 % Learning parameters with rate and maximum number of iterations
 alpha = 0.7;
 eta = 0.001;
-max_iter = 100;
-%min_error = 0;
-
-% actual output of each epoch
-out = zeros(1,200);
-iter = 0;
+max_iter = 100;     %Max number of iterations
+iter = 0;           %iteration counter
 
 while iter <= max_iter
     iter = iter + 1;
-    
-    %for i = 1:length(all_data)
+    % forward pass
+    y = weights * all_data;         %Bias part is included in both the weights and the data
+    %out = 2./(1 + exp(-y)) - 1;     %transfer to -1 and 1
         
-        % forward pass
-        y = all_data(1,:) * weights(1,1)...
-            + all_data(2,:) * weights(1,2);...
-            %+ all_data(3,:) * weights(1,3);                               %bias part
-        out = 2./(1 + exp(-y)) - 1;
-        
-        %backward pass
-        delta_out = (out - target).*(1 + out).*(1 - out)*0.5;
-        
-        %weight update
-        delta_weight(1,1) = (delta_weight(1,1) .* alpha) - (delta_out * all_data(1,:)') .* (1-alpha);
-        weights(1,1) = weights(1,1) + delta_weight(1,1) .* eta;
-        delta_weight(1,2) = (delta_weight(1,2) .* alpha) - (delta_out * all_data(2,:)') .* (1-alpha);
-        weights(1,2) = weights(1,2) + delta_weight(1,2) .* eta;
-        %bias weight update
-        delta_weight(1,3) = (delta_weight(1,3) .* alpha) - (delta_out * all_data(3,:)') .* (1-alpha);
-        weights(1,3) = weights(1,3) + delta_weight(1,3) .* eta;
-        
-    %end
+    %backward pass
+    %delta_out = (out - target).*(1 + out).*(1 - out)*0.5;
+    delta_out = (target - y);
+     
+    %weight update
+    weights(1,1) = weights(1,1) + eta.*delta_out*all_data(1,:)';
+    weights(1,2) = weights(1,2) + eta.*delta_out*all_data(2,:)';
+    weights(1,3) = weights(1,3) + eta.*delta_out*all_data(3,:)';  
 
-%Plotting of weights
-hold on
-axis([-20 20 -20 20])
-pointx = [(-20*weights(1,1) + weights(1,3)), (20*weights(1,1) + weights(1,3))];
-pointy = [20*weights(1,2),-20*weights(1,2)];
-line(pointx,pointy,'Color','black','LineStyle','-')
+    %Plotting of weights
+    hold on
+    axis([-20 20 -20 20])
+    pointx = [(-20*weights(1,1) + weights(1,3)), (20*weights(1,1) + weights(1,3))]; %Bias-weight is added to the x-value
+    pointy = [20*weights(1,2),-20*weights(1,2)];
+    line(pointx,pointy,'Color','black','LineStyle','-')
 
-pause(0.1)
+    weights
+    pause(0.1)
+
 end
 hold off
 
@@ -102,10 +90,10 @@ meansquare_error = mean(delta_out.^2)
 % Show misclassifications
 missclass = 0;
 for i = 1:length(target)
-    if i <= 100 && out(i) < 0
+    if i <= 100 && y(i) < 0
         missclass = missclass + 1;
         missclass_data = i;
-    elseif i > 100 && out(i) > 0
+    elseif i > 100 && y(i) > 0
         missclass = missclass + 1;
         missclass_data = i;
     end
@@ -113,7 +101,7 @@ end
 
 missclass
 
-%% delta learning
+
 
 
 
