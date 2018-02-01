101386b98c9accb3eac2d95a0d6df351b39a45b8
GIF1 <gif1@hi.is>
2018-02-01 16:59:47 +0100

    Comparison added
    Delta rule and perceptron learning comparisons with single layer

--- ANN_project1.m
+++ ANN_project1.m
@@ -22,35 +22,20 @@ sigma2 = [0.5,0;0,0.5];
 data2 = mvnrnd(mu2,sigma2,100);  %Produces multivariant normal distributed data
 
 % combine data into one matrix and add bias line in input
-all_data_original = [data1; data2];
-all_data_original = all_data_original';
-all_data_original = [all_data_original; ones(1,200)]; %All data including the bias line
+patterns = [data1; data2];
+patterns = [patterns'; ones(1,200)]; %All data including the bias line
 
 % Create an output matrix
-target = [ones(1,100), -ones(1,100)];   %first data group is 1 and second is -1
+targets = [ones(1,100), -ones(1,100)];   %first data group is 1 and second is -1
 
 % create a weight matrix
-[numDims, numInst] = size(all_data_original);
-numClasses = size(target,1);
-%weights = rand(numClasses, numDims);
-%delta_weight = zeros(numClasses, numDims);
+[numDims, numInst] = size(patterns);
+numClasses = size(targets,1);
 
 %shuffle data by random
 shuffle = randperm(200);
-all_data = all_data_original(:,shuffle);
-target = target(:,shuffle);
-
-%%          Plotting of data points
-
-% figure(1)
-% plot(all_data_original(1,1:100),all_data_original(2,1:100),'bo')
-% grid on
-% title('Data points for classification')
-% xlabel('X')
-% ylabel('Y')
-% 
-% hold on
-% plot(all_data_original(1,101:200),all_data_original(2,101:200),'r+')
+patternsShuf = patterns(:,shuffle);
+targets = targets(:,shuffle);
 
 %%          3.1.2 Single-layer perceptron
 
@@ -59,80 +44,56 @@ target = target(:,shuffle);
 % Learning parameters with rate and maximum number of iterations
 alpha = 0.7;
 eta = 0.001;
-max_iter = 100;     %Max number of iterations
-iter = 0;           %iteration counter
+
 
 etavec = [0.0001,0.001,0.005,0.01,0.1];
+epoch = [10 25 50 75 100];
 
-for eta_i = 1:length(etavec)
-    weights = rand(numClasses, numDims);
-    iter = 0; 
+weights = randn(1, numDims);
+
+plotId = 0;
+for epoch_i = 1:length(epoch)
     
     figure
-    plot(all_data_original(1,1:100),all_data_original(2,1:100),'bo')
-    grid on
-    title('Data points for classification')
-    xlabel('X')
-    ylabel('Y')
+    if plotId
+        plot(patterns(1,1:100),patterns(2,1:100),'bo')
+        grid on
+        title(['Single Layer - Delta Rule with Epoch = ' num2str(epoch(epoch_i))])
+        xlabel('X')
+        ylabel('Y')
 
-    hold on
-    plot(all_data_original(1,101:200),all_data_original(2,101:200),'r+')
-
-    h = animatedline;
-    while iter <= max_iter
-        iter = iter + 1;
-        % forward pass
-        out = weights * all_data;         %Bias part is included in both the weights and the data
-        %out = 2./(1 + exp(-y)) - 1;     %transfer to -1 and 1
-
-        %backward pass
-        %delta_out = (out - target).*(1 + out).*(1 - out)*0.5;
-        delta_out = (target - out);
-
-        %weight update
-        weights(1,1) = weights(1,1) + etavec(eta_i).*delta_out*all_data(1,:)';
-        weights(1,2) = weights(1,2) + etavec(eta_i).*delta_out*all_data(2,:)';
-        weights(1,3) = weights(1,3) + etavec(eta_i).*delta_out*all_data(3,:)';
-
-        %plotting data with bias
-        data_weights = weights(1,1:2);
-        threshold = -weights(1,3)/(data_weights*data_weights');      %normalised bias is threshold
-        norm_weights = sqrt(data_weights*data_weights');
-
-        %Plotting of weights
         hold on
-        %axis([-2 2 -2 2])
-        clearpoints(h);
-        x = 2.*[weights(1),weights(1)];
-        y = 2.*[weights(2),weights(2)];
-        x2 = 2.*[-weights(2),weights(2)];
-        y2 = 2.*[weights(1),-weights(1)];
-        xpoints = x*threshold + x2/norm_weights;
-        ypoints = y*threshold + y2/norm_weights;
-        addpoints(h,xpoints(1),ypoints(1));
-        addpoints(h,xpoints(2),ypoints(2));
-        drawnow;
-
-        %weights
-        pause(0.1)
-
+        plot(patterns(1,101:200),patterns(2,101:200),'r+')
+    else
+        title(['Single Layer - Delta Rule with Epoch = ' num2str(epoch(epoch_i))])
+        xlabel('Epoch')
+        ylabel('Misclassifications')
     end
-    hold off
+        
+    misclass_delta = ...
+        singleDeltaRule(patternsShuf, targets, eta, weights, epoch(epoch_i), plotId);
+    
+    if plotId
+        figure
+        plot(patterns(1,1:100),patterns(2,1:100),'bo')
+        grid on
+        title(['Single Layer - Perceptron Learning with Epoch = ' num2str(epoch(epoch_i))])
+        xlabel('X')
+        ylabel('Y')
 
-
-    % Calculate mean square error
-    meansquare_error = mean(delta_out.^2)
-    % Show misclassifications
-    missclass = 0;
-    for i = 1:length(target)
-        if target(i) > 0 && out(i) < 0
-            missclass = missclass + 1;
-            missclass_data = i;
-        elseif target(i) < 0 && out(i) > 0
-            missclass = missclass + 1;
-            missclass_data = i;
-        end
+        hold on
+        plot(patterns(1,101:200),patterns(2,101:200),'r+')
+    else
+        title(['Single Layer - Perceptron Learning with Epoch = ' num2str(epoch(epoch_i))])
+        xlabel('Epoch')
+        ylabel('Misclassifications')
     end
-
-    missclass
+    
+    misclass_percept = ...
+        singlePerceptronLearning(patternsShuf, targets, eta, weights, epoch(epoch_i), plotId);
+    
+    fprintf('\t\t\tMisclassifications\n')
+    fprintf('\t\t\tDelta Rule\tPerceptron Learning\n')
+    fprintf('Epoch %d:\t%d\t\t\t%d\n',...
+        epoch(epoch_i), misclass_delta, misclass_percept)
 end
---
+++ singlePerceptronLearning.m
@@ -0,0 +1,67 @@
+function missclass = singlePerceptronLearning(patterns, targets, eta, weights, epoch, plotId)
+
+max_iter = epoch;     %Max number of iterations
+iter = 0;           %iteration counter
+
+h = animatedline;
+while iter <= max_iter
+    iter = iter + 1;
+    % forward pass
+    out = weights * patterns;   %Bias part is included in both the weights and the data
+    
+    %backward pass
+    delta_out = (targets - sign(out));
+
+    %weight update
+    weights(1,1) = weights(1,1) + eta.*delta_out*patterns(1,:)';
+    weights(1,2) = weights(1,2) + eta.*delta_out*patterns(2,:)';
+    weights(1,3) = weights(1,3) + eta.*delta_out*patterns(3,:)';
+
+    if plotId
+        %plotting data with bias
+        data_weights = weights(1,1:2);
+        threshold = -weights(1,3)/(data_weights*data_weights');      %normalised bias is threshold
+        norm_weights = sqrt(data_weights*data_weights');
+
+        %Plotting of weights
+        hold on
+        clearpoints(h);
+        x = 2.*[weights(1),weights(1)];
+        y = 2.*[weights(2),weights(2)];
+        x2 = 2.*[-weights(2),weights(2)];
+        y2 = 2.*[weights(1),-weights(1)];
+        xpoints = x*threshold + x2/norm_weights;
+        ypoints = y*threshold + y2/norm_weights;
+        addpoints(h,xpoints(1),ypoints(1));
+        addpoints(h,xpoints(2),ypoints(2));
+        drawnow;
+    else
+        missclass = 0;
+        for i = 1:length(targets)
+            if targets(i) > 0 && out(i) < 0
+                missclass = missclass + 1;
+            elseif targets(i) < 0 && out(i) > 0
+                missclass = missclass + 1;
+            end
+        end
+        addpoints(h,iter-1,missclass);
+        drawnow;
+    end
+
+    %weights
+    pause(0.1)
+
+end
+hold off
+
+% Show misclassifications
+missclass = 0;
+for i = 1:length(targets)
+    if targets(i) > 0 && out(i) < 0
+        missclass = missclass + 1;
+    elseif targets(i) < 0 && out(i) > 0
+        missclass = missclass + 1;
+    end
+end
+
+end
